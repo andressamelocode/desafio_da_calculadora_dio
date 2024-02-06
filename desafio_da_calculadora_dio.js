0ea4d8de7d24dce0904bb07e25be653b629f8262
andressamelocode <dessmel@outlook.com>
2024-02-06 07:07:20 -0300

    desafio_da_calculadora_dio

--- desafio_da_calculadora_dio.js
+++ desafio_da_calculadora_dio.js
@@ -4,44 +4,23 @@ let nome = prompt("Digite seu nome: ");
 console.log("Seja bem-vindo(a) " + nome);
 
 
-let vitorias = prompt("Digite sua quantidade de experência XP: ");
+let vitorias = prompt("Digite sua quantidade de vitórias: ");
 let derrotas = prompt("Digite sua quantidade derrotas: ");
 
-function saldo(vitorias, derrotas) {
-    let saldo = vitorias - derrotas;
-    console.log("O seu saldo é: " + saldo)
-    }
+let saldo = vitorias - derrotas;
 
-    switch (saldo){
-        case saldo <10:
-        console.log("O Herói de nome " + nome + " tem de saldo " + saldo + " está no nível de ferro")   
-        break
-    
-        case 11 >= saldo || saldo <= 20:
-        console.log("O Herói de nome " + nome + " tem de saldo " + saldo + " está no nível de bronze")
-        break
-    
-        case 21 >= saldo || saldo <= 50:
-        console.log("O Herói de nome " + nome + " tem de saldo " + saldo + " está no nível de prata")
-        break
-    
-        case 51 >= saldo || saldo <= 80:
-        console.log("O Herói de nome " + nome + " tem de saldo " + saldo + " está no nível de ouro")
-        break
-    
-        case 81 >= saldo || saldo <= 90:
-        console.log("O Herói de nome " + nome + " tem de saldo " + saldo + " está no nível de diamante")
-        break
-    
-        case 91 >= saldo || saldo <= 100:
-        console.log("O Herói de nome " + nome + " tem de saldo " + saldo + " está no nível de lendário")
-        break
-    
-        case saldo >=101:
-        console.log("O Herói de nome " + nome + " tem de saldo " + saldo + " está no nível de imortal")
-        break
-    
-        default:
-        console.log("Erro! Digite apenas números.")
-    }
-
+if(saldo <= 10){
+    console.log("O herói de nome " + nome + " tem saldo " + saldo + " e está no nível de ferro")   
+}else if(11 >= saldo || saldo <= 20){
+    console.log("O herói de nome " + nome + " tem saldo " + saldo + " e está no nível de bronze")
+}else if(21 >= saldo || saldo <= 50){
+    console.log("O herói de nome " + nome + " tem saldo " + saldo + " e está no nível de prata")
+}else if(51 >= saldo || saldo <= 80){
+    console.log("O herói de nome " + nome + " tem saldo " + saldo + " e está no nível de ouro")
+}else if(81 >= saldo || saldo <= 90){
+    console.log("O herói de nome " + nome + " tem saldo " + saldo + " e está no nível de diamante")
+}else if(91 >= saldo || saldo <= 100){
+    console.log("O herói de nome " + nome + " tem saldo " + saldo + " e está no nível de lendário")
+}else{ (saldo >= 101)
+    console.log("O herói de nome " + nome + " tem saldo " + saldo + " e está no nível de imortal")
+}
